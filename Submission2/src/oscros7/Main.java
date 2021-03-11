c49187ed42dc181a6fea27a1f384d94cda7eda7d
Oscar Rosberg <oscros-7@student.ltu.se>
2021-03-11 12:52:08 +0100

    cleaned main

--- Submission2/src/oscros7/Main.java
+++ Submission2/src/oscros7/Main.java
@@ -7,17 +7,6 @@ public class Main {
 
         BankLogic bankLogic = new BankLogic();
         bankLogic.createCustomer("Oscar", "Rosberg", "1");
-        bankLogic.createCustomer("Oscar", "Rosberg", "2");
-        bankLogic.createSavingsAccount("1");
-        bankLogic.createCreditAccount("1");
-        bankLogic.deposit("1", 1001, 1000);
-        bankLogic.withdraw("1", 1002, 5001);
-        System.out.println(bankLogic.changeCustomerName("", "", "1"));
-       // System.out.println(bankLogic.getTransactions("1", 1001));
-        System.out.println(bankLogic.getCustomer("1"));
-
-
-        //System.out.println(bankLogic.getAccount("1", 1001));
 
     }
 }
